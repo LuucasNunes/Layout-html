ecbd10a2e77cb5ed42c2e74c637d2ef158b78b8f
Lucas da Rosa Nunes <182100519@senacinformatica.br>
2022-06-28 17:07:49 -0300

    Adding validation CPF and CNPJ

--- validation.js
+++ validation.js
@@ -4,11 +4,62 @@ function validar(){
 	var email = formUser.email.value;
 	var telefone = formUser.telefone.value;
 	var endereco = formUser.endereco.value;
+	var cpf = formUser.cpf.value;
+	var cnpj= formUser.cnpj.value;
 	var senha = formUser.senha.value;
 	
-	if(nome =="" || nome.length <=5){
+	if(nome == "" || nome.length <=5){
 		alert("Informe o nome completo!");
 		formUser.nome.focus();
 		return false;
 	}
+	if(email == "" || email.lenght <= 10){
+		alert("E-mail não atende os requisitos!");
+		formUser.email.focus();
+		return false;
+	}
+	
+	if(telefone == "" || telefone.length <=5){
+		alert("Informe o telefone com DDD!");
+		formUser.telefone.focus();
+		return false;
+	}
+	
+	if(endereco == "" || endereco.length <= 6){
+		alert("Informe o endereço completo!");
+		formUser.endereco.focus();
+		return false;
+	}
+	
+	if(senha == "" || senha.length <=5){
+		alert("Informe uma senha valida!");
+		formUser.senha.focus();
+		return false;
+	}
+	
+	if(cpf == "" && cnpj == ""){
+		alert("informe o CPF ou CNPJ");
+		formUser.cpf.focus();
+		return false;
+	}
+	
+	if(cpf != "" && cnpj != ""){
+		alert("Escolha o CPF ou CNPJ");
+		formUser.cpf.focus();
+		return false;
+	}
+	
+	if(cpf == "" && cnpj != ""){
+		if(cnpj.length !=18 ){
+		alert("CNPJ inválido");
+		formUser.cnpj.focus();
+		return false;
+	 }else if(cpf.length != 14){
+		alert("CPF inválido'!");
+		formUser.cpf.focus();
+		return false;
+	 }
+	}
+	
+	
 }
